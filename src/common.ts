a5109a0e50d115c877bca565b4f4cf6affd52d96
katzino <katzino@gmail.com>
2025-02-21 09:15:22 +0100

    fix: strip ticker symbol when validating

--- src/common.ts
+++ src/common.ts
@@ -3,9 +3,13 @@ import dayjs from 'dayjs';
 export const startDate = dayjs().add(-1, 'month').format('YYYY-MM-DD');
 
 export async function isTickerValid(input: string) {
-    const response = await fetch(`https://finance.yahoo.com/quote/${input}/`);
+    const response = await fetch(`https://finance.yahoo.com/quote/${stripTicker(input)}/`);
 
     return response.status === 200 && !response.redirected;
+}
+
+function stripTicker(input: string) {
+    return input.startsWith('$') ? input.slice(1) : input;
 }
 
 export function normalizeTicker(input: string) {
